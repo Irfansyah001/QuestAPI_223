1619d98c32e023927d660780f8acf06e482af245
Irfansyah001 <syahirfan545@gmail.com>
2025-12-18 22:28:30 +0700

    feat: Add ViewModel provider extension function
    Creates a new file `PenyediaViewModel.kt` to house a custom `CreationExtras` extension function named `aplikasiDataSiswa`.
    
    This function provides a convenient way to access the `AplikasiDataSiswa` instance from within a `ViewModelProvider` factory, simplifying dependency injection for ViewModels.

---
+++ app/src/main/java/com/example/questapi_223/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.questapi_223.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.questapi_223.repositori.AplikasiDataSiswa
+import com.example.questapi_223.viewmodel.EntryViewModel
+import com.example.questapi_223.viewmodel.HomeViewModel
+
+
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
+        )
