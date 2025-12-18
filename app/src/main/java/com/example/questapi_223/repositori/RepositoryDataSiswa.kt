684589a75a2f590a1d0db862f6c855475336f38a
Irfansyah001 <syahirfan545@gmail.com>
2025-12-18 22:08:55 +0700

    feat: Add repository interface for Siswa data
    Creates a new interface, `RepositoryDataSiswa`, to define the contract for data operations related to Siswa.
    
    - Adds the `getDataSiswa` function to retrieve a list of Siswa.
    - Adds the `postDataSiswa` function to send Siswa data.
    - Includes commented-out function definitions for future implementation: `getSatuSiswa`, `editSatuSiswa`, and `hapusSatuSiswa`.

---
+++ app/src/main/java/com/example/questapi_223/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,13 @@
+package com.example.questapi_223.repositori
+
+import com.example.questapi_223.apiservice.ServiceApiSiswa
+import com.example.questapi_223.modeldata.DataSiswa
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) : retrofit2.Response<Void>
+//    suspend fun getSatuSiswa(id:Int) : DataSiswa
+//    suspend fun editSatuSiswa(id:Int, dataSiswa: DataSiswa) : retrofit2.Response<Void>
+//    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
+}
+
