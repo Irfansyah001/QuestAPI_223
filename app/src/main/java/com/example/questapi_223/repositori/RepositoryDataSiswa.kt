1b0e2f02822d305b2ed844de1a950a8876b6e890
Irfansyah001 <syahirfan545@gmail.com>
2025-12-18 22:09:16 +0700

    feat: Implement network repository for student data
    Adds the `JaringanRepositoryDataSiswa` class, which implements the `RepositoryDataSiswa` interface.
    
    This class provides concrete implementations for fetching and posting student data by calling the respective methods on the `ServiceApiSiswa` service.

--- app/src/main/java/com/example/questapi_223/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_223/repositori/RepositoryDataSiswa.kt
@@ -11,3 +11,9 @@ interface RepositoryDataSiswa {
 //    suspend fun hapusSatuSiswa(id:Int):retrofit2.Response<Void>
 }
 
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+):RepositoryDataSiswa{
+    override suspend fun getDataSiswa() : List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa : DataSiswa):retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
