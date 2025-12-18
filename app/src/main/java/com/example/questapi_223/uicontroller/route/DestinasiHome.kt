69d7b3dae86db0325220268354574903389825da
Irfansyah001 <syahirfan545@gmail.com>
2025-12-18 22:30:49 +0700

    feat: Add `DestinasiHome` navigation destination
    Creates a new object `DestinasiHome` to define the "home" screen's navigation route and title resource.
    
    - Sets the route to `"home"`.
    - Assigns the title resource to `R.string.app_name`.

---
+++ app/src/main/java/com/example/questapi_223/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_223.uicontroller.route
+
+import com.example.questapi_223.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
