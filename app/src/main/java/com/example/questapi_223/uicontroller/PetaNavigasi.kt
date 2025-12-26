6a8f45b21822b14850dd62d9edb634e3abc64f4b
Irfansyah001 <syahirfan545@gmail.com>
2025-12-26 22:25:04 +0700

    feat: Add navigation route for the edit screen
    Adds a new composable to the `NavHost` in `PetaNavigasi.kt` to handle navigation to the edit screen.
    
    - Defines the route for `DestinasiEdit` using `routeWithArgs`.
    - Configures an integer navigation argument, `itemIdArg`, to pass the item ID to the `EditSiswaScreen`.
    - Implements the `navigateBack` action to return to the home destination and `onNavigateUp` for standard back navigation.

--- app/src/main/java/com/example/questapi_223/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_223/uicontroller/PetaNavigasi.kt
@@ -53,7 +53,17 @@ fun HostNavigasi(
                 navigateToEditItem = { navController.navigate("${DestinasiEdit.route}/$it") },
                 navigateBack = { navController.navigate(DestinasiHome.route) })
         }
-
+        composable(
+            DestinasiEdit.routeWithArgs, arguments = listOf(
+                navArgument(
+                    DestinasiEdit.itemIdArg
+                ) {
+                    type = NavType.IntType
+                })
+        ) {
+            EditSiswaScreen(
+                navigateBack = { navController.navigate(DestinasiHome.route) },
+                onNavigateUp = { navController.navigateUp() })
         }
     }
 }
