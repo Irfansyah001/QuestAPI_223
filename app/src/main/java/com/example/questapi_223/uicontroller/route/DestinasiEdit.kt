d089a15a3fa5f7fd7fe3eb14e8da990aa1471dcf
Irfansyah001 <syahirfan545@gmail.com>
2025-12-26 22:23:35 +0700

    feat: Add `DestinasiEdit` navigation destination
    Creates a new `DestinasiEdit` object to define the navigation route for the item editing screen.
    
    This object implements the `DestinasiNavigasi` interface and specifies the route `item_edit` and the title resource `R.string.edit_siswa`. It also defines a route with an argument, `idSiswa`, for navigating to a specific item.

--- app/src/main/java/com/example/questapi_223/uicontroller/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/questapi_223/uicontroller/route/DestinasiEdit.kt
@@ -2,4 +2,12 @@ package com.example.questapi_223.uicontroller.route
 
 import com.example.questapi_223.R
 
+object DestinasiEdit: DestinasiNavigasi {
 
+    override val route = "item_edit"
+    override val titleRes = R.string.edit_siswa
+
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+
+}
