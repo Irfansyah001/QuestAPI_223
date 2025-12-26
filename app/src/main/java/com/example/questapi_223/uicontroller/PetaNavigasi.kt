bb3adeacfcc35e311f24562eea1995f7a39025d3
Irfansyah001 <syahirfan545@gmail.com>
2025-12-26 22:24:15 +0700

    feat: Add detail screen navigation and route
    Updates the navigation graph to include a route for the detail screen.
    
    - Adds navigation from the `HomeScreen` to the `DestinasiDetail` screen, passing an item ID as an argument.
    - Removes the previous implementation for the `DestinasiEntry` route.

--- app/src/main/java/com/example/questapi_223/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_223/uicontroller/PetaNavigasi.kt
@@ -3,17 +3,23 @@ package com.example.questapi_223.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.questapi_223.uicontroller.route.DestinasiDetail
+import com.example.questapi_223.uicontroller.route.DestinasiEdit
 import com.example.questapi_223.uicontroller.route.DestinasiEntry
 import com.example.questapi_223.uicontroller.route.DestinasiHome
+import com.example.questapi_223.view.DetailSiswaScreen
+import com.example.questapi_223.view.EditSiswaScreen
 import com.example.questapi_223.view.EntrySiswaScreen
 import com.example.questapi_223.view.HomeScreen
 
 @Composable
-fun DataSiswaApp(navController: NavHostController = rememberNavController(),
-                 modifier: Modifier){
+fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier
+){
     HostNavigasi(navController = navController)
 }
 
@@ -21,18 +27,19 @@ fun DataSiswaApp(navController: NavHostController = rememberNavController(),
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-){
-    NavHost(navController = navController, startDestination = DestinasiHome.route,
-        modifier = Modifier){
+) {
+    NavHost(
+        navController = navController, startDestination = DestinasiHome.route, modifier = Modifier
+    ) {
         composable(DestinasiHome.route) {
-            HomeScreen(navigateToItemEntry = { navController.navigate (DestinasiEntry.route) },
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 navigateToItemUpdate = {
-//                    navController.navigate("${DestinasiDetail.route}/${it}")
+                    navController.navigate("${DestinasiDetail.route}/${it}")
                 })
         }
         composable(DestinasiEntry.route) {
-            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome
-                .route) })
+
         }
     }
 }
