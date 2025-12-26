fef62444a3f712fe1050d07eb737bc8360df70a8
Irfansyah001 <syahirfan545@gmail.com>
2025-12-27 02:59:09 +0700

    feat: Implement data fetching and UI state updates in EditViewModel
    This commit enhances the `EditViewModel` to fetch a single student's data upon initialization and provides a function to update the UI state.
    
    - An `init` block is added to fetch student data using `repositoryDataSiswa.getSatuSiswa(idSiswa)` within a `viewModelScope` coroutine.
    - The student ID is retrieved from `SavedStateHandle`.
    - A new `updateUiState` function is introduced to update the `uiStateSiswa` with new student details and validate the input.

--- app/src/main/java/com/example/questapi_223/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_223/viewmodel/EditViewModel.kt
@@ -21,4 +21,15 @@ RepositoryDataSiswa
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid = validasiInput(detailSiswa))
+    }
 
